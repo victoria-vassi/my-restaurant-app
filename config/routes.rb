eef643462e2782747146f34d6932373ce8e81ca4
victoria vassileva <victoria.vassileva@yahoo.fr>
2019-10-23 15:25:29 +0800

    Yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+  get 'restaurants/:id', to: 'restaurants#show'
+  get '/restaurants', to: 'restaurants#index'
+  get '/restaurants/new', to: 'restaurants#new', as: :new
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '0787317978',
+    category: 'Chinese',
+    stars: 5
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0787317978',
+    category: 'Italian',
+    rating: 5
+  },
+  {
+    name: 'Frog',
+    address: 'Covent Garden',
+    phone_number: '0787317978',
+    category: 'French',
+    rating: 4
+  },
+  {
+    name: 'Le Pain Quotidien',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0787317978',
+    category: 'Belgian',
+    rating: 3
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0787317978',
+    category: 'Italian',
+    rating: 2
+  },
+  {
+    name: 'Sushi Samba',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0787317978',
+    category: 'Japanese',
+    rating: 5
+  }
+]
