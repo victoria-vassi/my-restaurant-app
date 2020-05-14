d36fe704c758fc6d9d3c9ed917a9996142ee441c
victoria vassileva <victoria.vassileva@yahoo.fr>
2020-05-14 16:36:31 +0200

    added styling to buttons

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,7 +5,6 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
-    @reviews = @restaurant.reviews
   end
 
   def new
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-
+  validates :map, presence: true
   validates :name, uniqueness: true, presence: true
   validates :phone_number, presence: true
   validates :address, presence: true
   validates :category, inclusion: { in: [ 'British', 'Indian', 'Italian', 'Belgian', 'Japanese'] }
-
-  # geocoded_by :address
-  # after_validation :geocode, if: :will_save_change_to_address?
 end
