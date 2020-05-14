d3812c09090005fd25c03e994051d3285b555f94
victoria vassileva <victoria.vassileva@yahoo.fr>
2020-05-14 15:17:24 +0200

    Add dotenv - Protect my secret data in .env file

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,7 @@ class Restaurant < ApplicationRecord
   validates :phone_number, presence: true
   validates :address, presence: true
   validates :category, inclusion: { in: [ 'British', 'Indian', 'Italian', 'Belgian', 'Japanese'] }
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_060141) do
+ActiveRecord::Schema.define(version: 2020_05_14_131050) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_10_23_060141) do
     t.text "reviews"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "reviews", force: :cascade do |t|
