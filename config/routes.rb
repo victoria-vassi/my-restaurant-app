b49f36ebe378a42f19d2272007c6cccde88961ce
victoria vassileva <victoria.vassileva@yahoo.fr>
2020-05-20 10:26:30 +0200

    refactored roots paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :restaurants do
     resources :reviews, only: [:new, :create]
   end
   resources :reviews, only: [:show, :destroy]
 
-  get 'restaurants/:id', to: 'restaurants#show'
-  get '/restaurants', to: 'restaurants#index'
-  get '/restaurants/new', to: 'restaurants#new', as: :new
+  root to: 'restaurants#index'
 
-  get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit
-  patch '/restaurants/:id', to: 'restaurants#update'
+  resources :restaurants, only: [:show, :new, :edit, :update]
 end
